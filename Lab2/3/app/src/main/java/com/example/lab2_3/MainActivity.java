f303e3ee961a23fd538e546559dea16176bd7a57
Mariia <mariia.verhun@gmail.com>
2021-09-27 21:48:06 +0300

    lab2.3:v.0.1

--- Lab2/3/app/src/main/java/com/example/lab2_3/MainActivity.java
+++ Lab2/3/app/src/main/java/com/example/lab2_3/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.lab2_3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Spinner operationSpinner = findViewById(R.id.spinner);
+        ArrayAdapter <?> operationAdapter = ArrayAdapter.createFromResource(this,
+                R.array.operations,
+                android.R.layout.simple_spinner_item);
+        operationAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        operationSpinner.setAdapter(operationAdapter);
+
+        String selectedOperation = operationSpinner.getSelectedItem().toString();
+
     }
 }
