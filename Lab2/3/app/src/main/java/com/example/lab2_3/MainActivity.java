92d9bdd7817ff18d9a70caec696a01a680c3f22e
Mariia <mariia.verhun@gmail.com>
2021-09-28 21:25:32 +0300

    3 assign basic operation

--- Lab2/3/app/src/main/java/com/example/lab2_3/MainActivity.java
+++ Lab2/3/app/src/main/java/com/example/lab2_3/MainActivity.java
@@ -4,14 +4,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText et1, et2;
+    TextView result;
+    Button submitBtn;
+    String selectedOperation;
+
+    private String calculate (Integer num1, Integer num2, String operation) throws Exception {
+        switch (operation) {
+            case "+":
+                return Integer.toString(num1 + num2);
+            case "-":
+                return Integer.toString(num1 - num2);
+            case "/":
+                if (num2 != 0) return Integer.toString(num1 / num2);
+                else throw new Exception("Division to zero!");
+            case "*":
+                return Integer.toString(num1 * num2);
+            default:
+                return "";
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        submitBtn = (Button) findViewById(R.id.button);
+        et1 = (EditText) findViewById(R.id.editTextNumber1);
+        et2 = (EditText) findViewById(R.id.editTextNumber2);
+        result = (TextView) findViewById(R.id.result);
+
+
         final Spinner operationSpinner = findViewById(R.id.spinner);
         ArrayAdapter <?> operationAdapter = ArrayAdapter.createFromResource(this,
                 R.array.operations,
@@ -19,7 +51,21 @@ public class MainActivity extends AppCompatActivity {
         operationAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         operationSpinner.setAdapter(operationAdapter);
 
-        String selectedOperation = operationSpinner.getSelectedItem().toString();
+        submitBtn.setOnClickListener(view -> {
+            selectedOperation = operationSpinner.getSelectedItem().toString();
+            Integer num1 = Integer.parseInt(et1.getText().toString());
+            Integer num2 = Integer.parseInt(et2.getText().toString());
+            try {
+                result.setText(calculate(num1, num2, selectedOperation));
+            }
+            catch (Exception err) {
+                Toast.makeText(getApplicationContext(), err.toString() ,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
 
     }
 }
