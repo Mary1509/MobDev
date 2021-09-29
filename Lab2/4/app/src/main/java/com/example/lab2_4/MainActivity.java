5b72548721960d0e0c8b34b7ef9ee4bc87be0e09
Mariia <mariia.verhun@gmail.com>
2021-09-29 12:18:59 +0300

    tictactoe add ids

--- Lab2/4/app/src/main/java/com/example/lab2_4/MainActivity.java
+++ Lab2/4/app/src/main/java/com/example/lab2_4/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.lab2_4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +13,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner[][] apinnerArray = new Spinner[4][4];
+        Spinner[][] spinnerArray = new Spinner[4][4];
+        for (int i = 0; i < 4; i++){
+            for (int j = 0; j < 4; j++){
+                int id = getResources().getIdentifier(String.format("d%s%d", i, j),
+                        "id",
+                        getPackageName());
+                spinnerArray[i][j] = findViewById(id);
+
+            }
+        }
 
     }
 }
