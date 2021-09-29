53625ff80686ea9712d0769ae9112e9e5ebf1ac5
Mariia <mariia.verhun@gmail.com>
2021-09-29 11:39:07 +0300

    tictactoe init

---
+++ Lab2/4/app/src/main/java/com/example/lab2_4/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.lab2_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner[][] apinnerArray = new Spinner[4][4];
+
+    }
+}
