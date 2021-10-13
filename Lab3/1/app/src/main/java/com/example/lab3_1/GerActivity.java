583e4f4c335ebeb905905d74141aa45d063a42d6
Mariia <mariia.verhun@gmail.com>
2021-10-13 11:15:23 +0300

    Replace 1st task in folder

---
+++ Lab3/1/app/src/main/java/com/example/lab3_1/GerActivity.java
@@ -0,0 +1,64 @@
+package com.example.lab3_1;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class GerActivity extends AppCompatActivity {
+
+    Button back;
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_germany);
+
+        openText();
+        textView = findViewById(R.id.textView);
+        textView.setMovementMethod(new ScrollingMovementMethod());
+
+        back = findViewById(R.id.button);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent main = new Intent();
+                main.setClass(GerActivity.this, MainActivity.class);
+                startActivity(main);
+            }
+        });
+    }
+    public void openText(){
+
+        textView = findViewById(R.id.textView);
+        InputStream fin = null;
+        try  {
+            fin = getResources().openRawResource(R.raw.germany);
+            byte[] bytes = new byte[fin.available()];
+            int i = fin.read(bytes);
+            String text = new String(bytes);
+            textView.setText(text);
+        } catch (IOException ex) {
+            Toast.makeText(this, ex.getMessage(),
+                    Toast.LENGTH_SHORT).show();
+        }
+        finally {
+            try {
+                fin.close();
+            }
+            catch (IOException ex){
+                Toast.makeText(this, ex.getMessage(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+}
