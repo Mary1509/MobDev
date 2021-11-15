be86c928e664ee41f1dc883dd102d0b838cec27c
Mariia <mariia.verhun@gmail.com>
2021-11-15 02:46:33 +0200

    Sharpn, gauss + convolution in seperate function

--- Lab4/app/src/main/java/com/example/lab4/Task5.java
+++ Lab4/app/src/main/java/com/example/lab4/Task5.java
@@ -7,14 +7,15 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
-import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Arrays;
+
+
 
 public class Task5 extends AppCompatActivity {
     ImageView result;
@@ -23,13 +24,94 @@ public class Task5 extends AppCompatActivity {
 
     final int SIZE = 3;
 
-    int pix, newPix, alpha, red, green, blue;
+    RadioGroup radioGroup;
 
-    RadioButton blur, sharp, median, eroz, build, sobel;
+    void Convolution(int[][] filter, int div, int offset){
+        int A, R, G, B;
+        int sumR, sumG, sumB;
+        int[][] pixels = new int[SIZE][SIZE];
+        for (int y = 0; y < height - 2; y++)
+        {
+            for (int x = 0; x < width - 2; x++)
+            {
+                pixels[0][0] = img_bit.getPixel(x, y);
+                pixels[0][1] = img_bit.getPixel(x, y + 1);
+                pixels[0][2] = img_bit.getPixel(x, y + 2);
+                pixels[1][0] = img_bit.getPixel(x + 1, y);
+                pixels[1][1] = img_bit.getPixel(x + 1, y + 1);
+                pixels[1][2] = img_bit.getPixel(x + 1, y + 2);
+                pixels[2][0] = img_bit.getPixel(x + 2, y);
+                pixels[2][1] = img_bit.getPixel(x + 2, y + 1);
+                pixels[2][2] = img_bit.getPixel(x + 2, y + 2);
+
+                A = Color.alpha(img_bit.getPixel(1, 1));
+
+                R = ((((Color.red(pixels[0][0])* filter[0][0]) +
+                    (Color.red(pixels[1][0]) * filter[1][0]) +
+                    (Color.red(pixels[2][0]) * filter[2][0]) +
+                    (Color.red(pixels[0][1] )* filter[0][1]) +
+                    (Color.red(pixels[1][1] )* filter[1][1]) +
+                    (Color.red(pixels[2][1] )*filter[2][1]) +
+                    (Color.red(pixels[0][2] )* filter[0][2]) +
+                    (Color.red(pixels[1][2] )* filter[1][2]) +
+                    (Color.red(pixels[2][2])* filter[2][2]))
+                                / div) + offset);
+
+                if (R < 0)
+                {
+                    R = 0;
+                }
+                else if (R > 255)
+                {
+                    R = 255;
+                }
+
+                G = ((((Color.green(pixels[0][0])* filter[0][0]) +
+                        (Color.green(pixels[1][0]) * filter[1][0]) +
+                        (Color.green(pixels[2][0]) * filter[2][0]) +
+                        (Color.green(pixels[0][1] )* filter[0][1]) +
+                        (Color.green(pixels[1][1] )* filter[1][1]) +
+                        (Color.green(pixels[2][1] )*filter[2][1]) +
+                        (Color.green(pixels[0][2] )* filter[0][2]) +
+                        (Color.green(pixels[1][2] )* filter[1][2]) +
+                        (Color.green(pixels[2][2])* filter[2][2]))
+                        / div) + offset);
+
+                if (G < 0)
+                {
+                    G = 0;
+                }
+                else if (G > 255)
+                {
+                    G = 255;
+                }
+
+                B = ((((Color.blue(pixels[0][0])* filter[0][0]) +
+                        (Color.blue(pixels[1][0]) * filter[1][0]) +
+                        (Color.blue(pixels[2][0]) * filter[2][0]) +
+                        (Color.blue(pixels[0][1] )* filter[0][1]) +
+                        (Color.blue(pixels[1][1] )* filter[1][1]) +
+                        (Color.blue(pixels[2][1] )*filter[2][1]) +
+                        (Color.blue(pixels[0][2] )* filter[0][2]) +
+                        (Color.blue(pixels[1][2] )* filter[1][2]) +
+                        (Color.blue(pixels[2][2])* filter[2][2]))
+                        / div) + offset);
+
+                if (B < 0)
+                {
+                    B = 0;
+                }
+                else if (B > 255)
+                {
+                    B = 255;
+                }
+                result_bit.setPixel(x + 1, y + 1, Color.argb(A, R, G, B));
+            }
+        }
+    }
 
     void blurFilter(){
         int A, R, G, B;
-//        int sumR, sumG, sumB;
         int[][] pixels = new int[SIZE][SIZE];
 
         int[][] filter = new int[][] {
@@ -37,23 +119,6 @@ public class Task5 extends AppCompatActivity {
                 { 2, 4, 2 },
                 { 1, 2, 1 }
         };
-//        double[][] filter = new double[][] {
-//                { 1, 1, 1 },
-//                { 1, 1, 1 },
-//                { 1, 1, 1 }
-//        };
-
-//                double[][] filter = new double[][] {
-//                { 0.077847, 0.123317, 0.077847 },
-//                { 0.123317, 0.195346, 0.123317 },
-//                { 0.077847, 0.123317, 0.077847 }
-//        };
-
-//        double[][] filter = new double[][] {
-//                { -1, -1, -1 },
-//                { -1, 9, -1 },
-//                { -1, -1, -1 }
-//        };
 
         int div = 16;
         int offset = 0;
@@ -84,6 +149,8 @@ public class Task5 extends AppCompatActivity {
                 }
 
         }
+
+//        Convolution(filter, div, offset);
         result.setImageBitmap(result_bit);
     }
 
@@ -97,13 +164,9 @@ public class Task5 extends AppCompatActivity {
                 { -1, 9, -1 },
                 { -1, -1, -1 }
         };
-//        double[][] filter = new double[][] {
-//                { 0, -1, 0 },
-//                { -1, 5, -1 },
-//                { 0, -1, 0 }
-//        };
 
         int div = 1;
+        int offset = 0;
 
         for(int y = 0; y < height - 2; y++) {
             for(int x = 0; x < width - 2; x++) {
@@ -136,6 +199,7 @@ public class Task5 extends AppCompatActivity {
             }
 
         }
+//        Convolution(filter, div, offset);
         result.setImageBitmap(result_bit);
     }
 
@@ -162,9 +226,9 @@ public class Task5 extends AppCompatActivity {
                 Arrays.sort(green_pixels);
                 Arrays.sort(blue_pixels);
 
-                R = red_pixels[(red_pixels.length+1) / 2];
-                G = green_pixels[(green_pixels.length+1) / 2];
-                B = blue_pixels[(blue_pixels.length+1) / 2];
+                R = (red_pixels[(red_pixels.length) / 2] + red_pixels[(red_pixels.length+1) / 2])/2;
+                G = (green_pixels[(green_pixels.length) / 2] + green_pixels[(green_pixels.length+1) / 2])/2;
+                B = (blue_pixels[(blue_pixels.length) / 2] + blue_pixels[(blue_pixels.length+1) / 2])/2;
 
 
                 // apply new pixel
@@ -195,24 +259,16 @@ public class Task5 extends AppCompatActivity {
 
         result = findViewById(R.id.imageView2);
         img_bit = BitmapFactory.decodeResource(getResources(),
-                R.drawable.p1);
+                R.drawable.photo3);
         width = img_bit.getWidth();
         height = img_bit.getHeight();
         result_bit = Bitmap.createBitmap(width, height, img_bit.getConfig());
 
-        blur = findViewById(R.id.radioButton6);
-        sharp = findViewById(R.id.radioButton7);
-        median = findViewById(R.id.radioButton8);
-        eroz = findViewById(R.id.radioButton9);
-        build = findViewById(R.id.radioButton10);
-        sobel = findViewById(R.id.radioButton11);
-
-
-        View.OnClickListener radioOnClickListener = new View.OnClickListener() {
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                RadioButton rb = (RadioButton)v;
-                switch (rb.getId()){
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
                     case R.id.radioButton6: blurFilter();
                         break;
                     case R.id.radioButton7: sharpFilter();
@@ -227,30 +283,7 @@ public class Task5 extends AppCompatActivity {
                         break;
                 }
             }
-        };
-
-        blur.setOnClickListener(radioOnClickListener);
-        sharp.setOnClickListener(radioOnClickListener);
-        median.setOnClickListener(radioOnClickListener);
-        eroz.setOnClickListener(radioOnClickListener);
-        build.setOnClickListener(radioOnClickListener);
-        sobel.setOnClickListener(radioOnClickListener);
-
-//        double[][] filter = new double[][] {
-//                { 1,  4,  6,  4,  1 },
-//                { 4, 16, 24, 16,  4},
-//                {6, 24, 36, 24,  6},
-//                {4, 16, 24, 16,  4},
-//                {1,  4,  6,  4,  1}
-//        };
-
-
-
-
-
-
-
-
+        });
     }
     @Override
     public void onBackPressed() {
