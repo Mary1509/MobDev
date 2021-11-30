80b1a6d7096da877a001f6a9a00a950ef88dc3cc
Mariia <mariia.verhun@gmail.com>
2021-11-30 23:17:02 +0200

    Example 1 with rectangle

--- Lab5/1/app/src/main/java/com/example/lab5_1/MyView.java
+++ Lab5/1/app/src/main/java/com/example/lab5_1/MyView.java
@@ -1,27 +1,53 @@
 package com.example.lab5_1;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.View;
-
 public class MyView extends View {
+    int x = 0; // Координата по горизонталі лівого верхнього кута
+    //прямокутника
+    int widthRect = 300; // Ширина прямокутника
+    int vx = 10; // Скорость по оси Ox
+
     public MyView(Context context) {
         super(context);
     }
+
     @Override
-    public void onDraw(Canvas canvas) {
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
         Paint paint = new Paint();
-        paint.setColor(Color.WHITE);
-        paint.setTextSize(100);
-        paint.setAntiAlias(true);
-        canvas.drawColor(Color.BLUE);
-        canvas.drawText("Hello Kitty", 200, 150, paint);
-        Bitmap image = BitmapFactory.decodeResource(getResources(),
-                R.drawable.photo2);
-        canvas.drawBitmap(image, 40, 200, null);
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(20);
+        // Рухається одна сторона
+//        x++;
+//        canvas.drawRect(x,200,300,500,paint);
+//        invalidate();
+
+        // Рухається без зупинки
+//        x = x + 10;
+//        canvas.drawRect(x,200,300 + x,500,paint);
+//        invalidate();
+
+        // Рухається до однієї сторони екрану
+
+//        int widthRect = 300;
+//        x = x + 10;
+//        if (x > canvas.getWidth() - widthRect)
+//            x = canvas.getWidth() - widthRect;
+//        canvas.drawRect(x,200,widthRect + x,500,paint);
+//        invalidate();
+
+        // Рухіється між "стінками екрану"
+        x = x + vx;
+        if (x > canvas.getWidth() - widthRect) vx = vx * -1;
+        if (x < 0) vx = vx * -1;
+        canvas.drawRect(x,200,widthRect + x,500,paint);
+        invalidate();
+
     }
 }
