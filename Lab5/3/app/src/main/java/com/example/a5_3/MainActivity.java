9b0c4a7c2bf831e17107a2362d3abe27f8cce49e
Mariia <mariia.verhun@gmail.com>
2021-11-30 23:53:08 +0200

    Example 3

---
+++ Lab5/3/app/src/main/java/com/example/a5_3/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a5_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new MyDrawView(this));
+    }
+}
