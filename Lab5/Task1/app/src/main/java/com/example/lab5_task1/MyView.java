1bb1971fc5a77167645867f27eb00dcda12c5530
Mariia <mariia.verhun@gmail.com>
2021-12-01 23:50:37 +0200

    First animation, further speed change required

--- Lab5/Task1/app/src/main/java/com/example/lab5_task1/MyView.java
+++ Lab5/Task1/app/src/main/java/com/example/lab5_task1/MyView.java
@@ -11,39 +11,37 @@ public class MyView extends View {
     public MyView(Context context) {
         super(context);
     }
-
     Paint paint = new Paint();
 
+    int radius = 500;
+
+    double alpha = 0;
+    int a = 300;
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+
         int centerX = getWidth() /2;
         int centerY = getHeight() /2;
-        int radius = 500;
-
-        int x = centerX + radius;
-
-        double alpha = Math.PI / 3;
-        double k = Math.tan(alpha);
-
-        int y = (int)(k*x);
-
-        int a = 300;
-
-        int centerRectX = centerX + radius;
-        int centerRectY = centerY;
 
 
+        alpha += Math.PI / 200;
+        if (alpha > 2 * Math.PI){
+            alpha -= 2 * Math.PI;
+        }
+
+        int centerRectX = (int)(centerX + radius * Math.cos(alpha));
+        int centerRectY = (int)(centerY - radius * Math.sin(alpha));
         paint.setColor(Color.BLUE);
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeWidth(10);
-
         canvas.drawRect(centerRectX - a/2, centerRectY - a/2, centerRectX + a/2, centerRectY+a/2, paint);
         canvas.drawCircle(centerX, centerY, radius, paint);
         paint.setColor(Color.RED);
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeWidth(30);
         canvas.drawPoint(centerRectX, centerRectY, paint);
+        invalidate();
     }
 
 }
